a13fa8aab829b3b93c9d2d5a237fabe9e66ee245
fazelke <keya.fazelinia@verizonwireless.com>
2018-08-26 00:45:54 -0500

    implemented  merge sort

---
+++ Sorting/mergeSort.js
@@ -0,0 +1,22 @@
+
+const mergeSort = (arr) => {
+  if (arr.length < 2) return arr
+
+  const mid = Math.floor(arr.length / 2),
+    left = arr.slice(0, mid),
+    right = arr.slice(mid)
+
+  return merge(mergeSort(left), mergeSort(right))
+}
+
+
+const merge = (left, right) => {
+  const result = []
+
+  while (left.length || right.length) {
+    if (!right.length || (left.length && left[0] < right[0])) result.push(left.shift())
+    else result.push(right.shift())
+  }
+
+  return result
+}
